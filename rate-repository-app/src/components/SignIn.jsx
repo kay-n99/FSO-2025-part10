ce074be87d908d4f6b639a54a807c361ddf27859
kayn01 <kayanadonis@gmail.com>
2025-08-19 12:11:22 +0700

    exercises 10.14

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import theme from "../theme";
+import useSignIn from "../hooks/useSignIn";
 
 const styles = StyleSheet.create({
   container: {
@@ -47,8 +48,17 @@ const initialValues = {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn()
+
+  const onSubmit = async (values) => {
+    const { username, password}  = values
+
+    try {
+      const { data } = await signIn({username, password})
+      console.log(data)
+    }catch(e){
+      console.log(e)
+    }
   };
 
   return (
